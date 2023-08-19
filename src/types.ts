58da77d9156d42efc583c59daf55189caac21a68
bauerbrun0 <bauerbruno@protonmail.com>
2023-08-19 18:13:41 +0200

    Added 'DetailedMediaItem' types

--- src/types.ts
+++ src/types.ts
@@ -34,6 +34,50 @@ export interface MediaItem extends BaseMediaItem {
     tagline: string | null;
 }
 
+interface BaseDetailedMediaItem extends BaseMediaItem {
+    genres: Genre[];
+    homepage: string | null;
+    originalLanguage: string;
+    originalTitle: string;
+    spokenLanguages: string[];
+    status: string | null;
+    voteCount: number | null;
+    tagline: string | null;
+}
+
+export interface BaseDetailedMovie extends BaseDetailedMediaItem {
+    imdbId: string;
+    runtime: number | null;
+    budget: number | null;
+    revenue: number | null;
+}
+
+export interface DetailedMovie extends BaseDetailedMovie {
+    logoPath: string | null;
+    trailers: Video[];
+    teasers: Video[];
+    actors: Actor[];
+    directors: CrewMember[];
+    producers: CrewMember[];
+}
+
+export interface BaseDetailedTvShow extends BaseMediaItem {
+    inProduction: boolean;
+    creators: Creator[];
+    episodeRuntimes: number[];
+    numberOfEpisodes: number | null;
+    numberOfSeasons: number | null;
+    seasons: Season[];
+}
+
+export interface DetailedTvShow extends BaseDetailedTvShow {
+    imdbId: string;
+    logoPath: string | null;
+    trailers: Video[];
+    teasers: Video[];
+    actors: Actor[];
+}
+
 export enum Language {
     English = "en-US",
     Hungarian = "hu-HU",
@@ -49,6 +93,43 @@ export enum MediaType {
     TvShow = 'tv',
 }
 
+export interface Genre {
+    id: number;
+    name: string;
+}
+
+export interface Season {
+    id: number;
+    airDate: string | null;
+    episodeCount: number | null;
+    name: string | null;
+    seasonNumber: number;
+}
+
+export interface Video {
+    name: string;
+    youtubeLink: string;
+}
+
+// To paraphrase Quentin Tarantino:
+// https://youtu.be/h3-va0umXTY?si=3ycJbLYKxulklL1_&t=178
+export interface Actor {
+    name: string;
+    character: string;
+    profilePath: string | null;
+}
+
+export interface CrewMember {
+    name: string;
+    profilePath: string | null;
+    job: string;
+}
+
+export interface Creator {
+    name: string;
+    profilePath: string | null;
+}
+
 export enum ErrorStatus {
     BadRequest = 400,
     InternalServerError = 500,
@@ -57,9 +138,4 @@ export enum ErrorStatus {
 export interface Logo {
     logoPath: string;
     language: string | null;
-}
-
-export interface Genre {
-    id: number;
-    name: string;
 }
